4f69230acf0eb75f3516910a749993f9a3336890
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-14 21:21:36 -0500

    cleaning up flow

--- textnow/api/TextNowAPI.py
+++ textnow/api/TextNowAPI.py
@@ -5,11 +5,17 @@ import cloudscraper
 
 from textnow.enum import MessageType, MessageDirection, ContactType, ReadStatus
 from textnow.model.Client import Client
+from textnow.util.ConfigReader import ConfigReader
 
 
 class TextNowAPI:
     def __init__(self):
         self.__scraper = cloudscraper.create_scraper()
+
+        self.__BASE_URL = ConfigReader.get("api", "textnow_base_url")
+        self.__API_ROUTE = ConfigReader.get("api", "api_route")
+        self.__MESSAGES_ROUTE = ConfigReader.get("api", "messages_route")
+        self.__USERS_ROUTE = ConfigReader.get("api", "users_route")
 
     def send_message(self, message: str, send_to: str) -> None:
         client_config = Client.get_client_config()
@@ -27,8 +33,9 @@ class TextNowAPI:
 
         data = {"json": json.dumps(json_data)}
 
-        response = self.__scraper.post(f"https://www.textnow.com/api/users/{client_config.username}/messages",
-                                       headers=client_config.headers,
-                                       cookies=client_config.cookies,
-                                       data=data)
+        response = self.__scraper.post(
+            f"{self.__BASE_URL}{self.__API_ROUTE}{self.__USERS_ROUTE}/{client_config.username}{self.__MESSAGES_ROUTE}",
+            headers=client_config.headers,
+            cookies=client_config.cookies,
+            data=data)
         response.raise_for_status()
